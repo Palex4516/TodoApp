25fbabc2d7e2bd0c502283a14c4ce6aec3a4c713
Alexander Norgren <alexandernorgren2@gmail.com>
2024-05-15 13:10:07 +0200

    Update Todo.razor, TodoService.cs, and ToDoList.csproj

--- Services/TodoService.cs
+++ Services/TodoService.cs
@@ -13,7 +13,7 @@ namespace ToDoList.Services
             if (todoList != null)
             {
                 todoList.Sort((x, y) => x.Id < y.Id ? -1 : 1);
-                todoList.ForEach(x => Console.WriteLine($"Id: {x.Id}, Name: {x.Name}, IsComplete: {x.IsComplete}"));
+                todoList.ForEach(x => Console.WriteLine($"Id: {x.Id}, Name: {x.Name}, Done: {x.Done}"));
                 return todoList;
             }
             else
@@ -52,7 +52,7 @@ namespace ToDoList.Services
         {
             var svar = await client.PutAsJsonAsync(_base_url + "/" + todoItem.Id, todoItem);
             svar.EnsureSuccessStatusCode();
-            Console.WriteLine($"Updated TodoItem, Id: {todoItem.Id}, Name: {todoItem.Name}, IsComplete: {todoItem.IsComplete}");
+            Console.WriteLine($"Updated TodoItem, Id: {todoItem.Id}, Name: {todoItem.Name}, Done: {todoItem.Done}");
         }
     }
 }
